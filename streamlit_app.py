b9a5a0a256e26d23e62b1b2ff1035e52db289098
ahlemelayeb-tech <ahlem.elayeb@smile.fr>
2026-06-08 17:13:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,9 @@ if Ingriendts_list:
 
 
 
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
 
 
